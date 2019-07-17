e415342fb571b063025f6a29538b584f3315978a
Harry Tran <duchoang.vp@gmail.com>
2019-07-17 08:42:28 +0700

    Initial Commit

---
+++ Spider/Spider.h
@@ -0,0 +1,19 @@
+//
+//  Spider.h
+//  Spider
+//
+//  Created by Harry Tran on 7/17/19.
+//  Copyright © 2019 Harry Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Spider.
+FOUNDATION_EXPORT double SpiderVersionNumber;
+
+//! Project version string for Spider.
+FOUNDATION_EXPORT const unsigned char SpiderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Spider/PublicHeader.h>
+
+
